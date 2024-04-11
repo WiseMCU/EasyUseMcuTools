480ef28b27a50e25e9bf4c07459a2c9890c61ba5
ouyangyunpeng <yunpeng.ouyang@snibe.cn>
2024-04-11 10:49:41 +0800

    perf:添加RTT工具支持

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # EasyUseMcuTools
 个人使用的MCU工具和驱动集合库
 
-- rtt_log - RTT打印小工具
+- rtt_log - RTT打印工具 (已添加，单独功能在子分支)
---
+++ middlewares.c
@@ -0,0 +1,16 @@
+/**
+ * @file      middlewares.h
+ * @brief     集合库配置文件
+ * @version   1.0
+ * @date      2024-04-11
+ * @author    WiseMCU
+ */
+
+/* Tools --------------------------------------------------------------------*/
+/* RTT打印工具 */
+#include "middlewares\rtt_log\core_printf.c"
+#include "middlewares\rtt_log\SEGGER_RTT_printf.c"
+#include "middlewares\rtt_log\SEGGER_RTT.c"
+
+
+/********************************* END OF FILE ********************************/
